af479b6ed94245e74599aea3aa4f51d3a12d8ede
Bancie <144613141+Bancie@users.noreply.github.com>
2024-06-21 19:44:09 +0700

    pipp

--- main/sche.cpp
+++ main/sche.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define var 5 // <-- Variables Summarizing in Data File
 
 // Scanning Data File
-void readfile(int a[maxm][maxn], int &job_amount, int &prec, int &psum, int &job_scale) {
+void readfile (int a[maxm][maxn], int &job_amount, int &prec, int &psum, int &job_scale) {
 	FILE *f;
 	f=fopen("/Users/chibangnguyen/Documents/schedulang/data/data inp/na.inp","rt");
     fscanf(f, "%d", &job_amount);
@@ -22,19 +22,37 @@ void readfile(int a[maxm][maxn], int &job_amount, int &prec, int &psum, int &job
 }
 
 // Process Variable
-double process (int i, int a[maxm][maxn], int &job_amount, int &job_scale) {
+double process (int i, int a[maxm][maxn], int job_amount, int job_scale) {
     double p = (double) (a[i][1]*job_amount)/job_scale;
     return p;
 }
 
-// P-factor Variable
-double p_factor (int i, int a[maxm][maxn], double p) {
-    double p_factor = (double) a[i][4]/p;
-    return p_factor;
+// Non-precedence p-factor Variable
+double p_factor_nonprec (int i, int a[maxm][maxn], double p) {
+    double p_factor_nonprec = (double) a[i][4]/p;
+    return p_factor_nonprec;
 }
 
+// Precedence p-factor Variable
+double p_factor_prec_tu (int i, int a[maxm][maxn], double p) {
+    if (i<=0) {
+        return 0;
+    }
+    return a[i][4] + p_factor_prec(i-1,a,p);
+}
+double p_factor_prec_mau (int i, int a[maxm][maxn], int job_amount, int job_scale, double p) {
+    if (i<=0) {
+        return 0;
+    }
+    return process(i,a,job_amount,job_scale) + process(i-1,a,job_amount,job_scale);
+}
+double p_factor_prec (int i, int a[maxm][maxn], int job_amount, int job_scale, double p) {
+    return p_factor_prec_tu(i,a,p)/p_factor_prec_mau(i,a,job_amount,job_scale,p);
+}
+
+
 // Printing Output File
-void writefile(int a[maxm][maxn], int job_amount, int prec, int psum, int job_scale) {
+void writefile (int a[maxm][maxn], int job_amount, int prec, int psum, int job_scale) {
 	FILE *f;
 	f=fopen("/Users/chibangnguyen/Documents/schedulang/data/data out/na.out","wt");
 	fprintf(f, "%d\n", job_amount);
@@ -42,11 +60,15 @@ void writefile(int a[maxm][maxn], int job_amount, int prec, int psum, int job_sc
     fprintf(f, "%d\n", psum);
     fprintf(f, "%d\n", job_scale);
     for (int i=0; i<job_amount; i++) {
-        //double p_factor = (double) a[i][4]/p;
+        //double p_factor_nonprec = (double) a[i][4]/p;
         for (int j=0; j<var; j++) {
             fprintf(f, "%d ", a[i][j]);
         }
-        fprintf(f, "%.4lf %.4lf\n", process(i,a,job_amount,job_scale), p_factor(i,a,process(i,a,job_amount,job_scale)));
+        if (prec==0) {
+            fprintf(f, "%.4lf %.4lf\n", process(i,a,job_amount,job_scale), p_factor_nonprec(i,a,process(i,a,job_amount,job_scale)));
+        } else if (prec==1) {
+            fprintf(f, "%.4lf %.4lf\n", process(i,a,job_amount,job_scale), p_factor_nonprec(i,a,process(i,a,job_amount,job_scale)));
+        }
     }
 	fclose(f);
 }
