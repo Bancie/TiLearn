75173602c39c8fd3b34262c9af00de10d0596192
Bancie <144613141+Bancie@users.noreply.github.com>
2024-07-28 15:06:51 +0700

    v1.0

---
+++ main/phase 2/phase_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std; 
+#define maxm 1000
+#define maxn 1000
+#define var 5 // <-- Variable Summarized from Data File
+
+class Job_Type {
+    public:
+    int a[maxm][maxn];
+    int prec;
+    int plan_time;
+    char line[maxm];
+    // SCANNING DATA FILE <---- NA.OUT
+    void readfile () {
+        FILE *f;
+        f=fopen("/Users/chibangnguyen/Documents/TiLearn/data/na.out","rt");
+            fscanf(f, "%d", &prec);
+            for (int i=0; i<maxm; i++) {
+                for (int j=0; j<var; j++) {
+                    fscanf(f, "%d", &a[i][j]);
+                }
+            }
+        fclose(f);
+    }
+
+    // PRINTING OUTPUT FILE ----> NON-PRECEDENCE SORTING
+    void writefile () {
+        FILE *f;
+        double sum=0;
+        f=fopen("/Users/chibangnguyen/Documents/TiLearn/data/nonprec_sorting.out","wt");
+        fclose(f);
+    }
+};
+
+int main () {
+    Job_Type type1;
+    type1.readfile();
+    type1.writefile();
+	return 0;
+} 
