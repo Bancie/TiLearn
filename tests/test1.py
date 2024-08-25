f2373b1921f590604304578f4d8ec3c5c043b06c
Bancie <144613141+Bancie@users.noreply.github.com>
2024-08-25 18:45:24 +0700

    upgraded path

--- tests/test1.py
+++ tests/test1.py
@@ -1,4 +1,5 @@
-import tilearn
+import tilearn as tl
+import process as pc
 
 x = [[1, 2, 3, 6, 3],[2, 4, 8, 4, 2],[2, 4, 8, 6, 7],[2, 4, 8, 4, 2],[2, 4, 8, 4, 2]]
 
--- venv/lib/python3.12/site-packages/tilearn/__init__.py
+++
@@ -1,3 +0,0 @@
-from tilearn.src.basis import *
-from tilearn.src.process import *
-from tilearn.src.factor import *
