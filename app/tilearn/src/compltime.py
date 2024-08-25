e81ba72b0a131a74d954224eaca147a9b8265e22
Bancie <144613141+Bancie@users.noreply.github.com>
2024-08-25 15:58:58 +0700

    compltime 1.0

--- app/tilearn/src/compltime.py
+++ app/tilearn/src/compltime.py
@@ -1,11 +1,34 @@
-from tilearn import *
+import tilearn as tl
+import numpy as np
 
-x = [[1, 2, 3],
-     [2, 4, 8],
-     [2, 4, 8]]
+x = [[1, 2, 3, 6, 3],[2, 4, 8, 4, 2],[2, 4, 8, 6, 7]]
+var = 5
+maxm, maxn = 1000, 1000
+a = [[0 for i in range(5)] for j in range(5)]
 
-print(job_amout(x))
+def non_pre(a, prec, plan_time):
+     for i in range(tl.job_amount(a)):
+          for j in range(var):
+               print(a[i][j])
 
-#def non_pre(a):
-#    for i in range(job_amount(a)):  
-        
+          if prec == 0:
+               print(tl.process(
+                    i, a, plan_time,
+                    tl.jobscale(
+                         a, tl.job_amount(a)
+                         )
+                    ),
+                    tl.p_factor_nonprec(
+                         i, a,
+                         tl.process(
+                              i, a, plan_time,
+                              tl.jobscale(
+                                   a, tl.job_amount(a)
+                                   )
+                              )
+                         )
+                    )
+               
+#non_pre(x, 0, 10)
+
+#print(a)
--- tests/test1.py
+++ tests/test1.py
@@ -5,4 +5,17 @@ x = [[1, 2, 3],
      [1, 2, 3],
      [2, 4, 6]]
 
-print(tilearn.job_amount(x))
+# print(tilearn.job_amount(x))
+
+rows = 3
+cols = 4
+two_d_list = []
+
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        row.append(0)
+    two_d_list.append(row)
+
+for row in two_d_list:
+    print(row)
