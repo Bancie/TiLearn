b06e8818fad8e413c95b4cd32e2f25ba3217d959
Bancie <144613141+Bancie@users.noreply.github.com>
2024-08-27 20:11:42 +0700

    upgraded

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("app/README.md", "r") as f:
 
 setup(
     name="TiLearn",
-    version="0.0.10",
+    version="0.0.12",
     description="An open-source project for time management and scheduling solutions.",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Bancie/TiLearn",
+    url="https://bancie.github.io/TiLearn/",
     author="Bancie Ng",
     author_email="chibangn1@gmail.com",
     license="MIT",
@@ -25,9 +25,13 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    # install_requires=["bson >= 0.5.10"],
-    extras_require={
-        "dev": ["twine>=5.1.1"],
+    project_urls={  # This dictionary contains the project links
+        'Homepage': 'https://bancie.github.io/TiLearn/',
+        'Source': 'https://github.com/Bancie/TiLearn',
     },
-    python_requires=">=3.12.5",
+    # install_requires=["bson >= 0.5.10"],
+    # extras_require={
+        # "dev": ["twine>=5.1.1"],
+    # },
+    python_requires=">=3.10.0",
 )
