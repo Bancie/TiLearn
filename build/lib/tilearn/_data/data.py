9b7b2d54c4c5a5fa3a718226369c516811d2ac28
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-05 00:07:42 +0700

    _data upgraded

--- build/lib/tilearn/_data/data.py
+++ build/lib/tilearn/_data/data.py
@@ -1,10 +1,10 @@
 import csv
-import tilearn as tl
 
 def read_file(file_path):
     data = []
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
+        next(reader)
         for row in reader:
             for i in range(1, 5):
                 row[i] = int(row[i])
