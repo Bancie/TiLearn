167a38f03a60974f96c8e40ddab0c984d5294f15
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-09 22:47:12 +0700

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -3,8 +3,8 @@ from tilearn import _plat as pl
 
 lists = [
     pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data2.csv', prec=1),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data3.csv', prec=1)
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data2.csv', prec=0),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data3.csv', prec=0)
 ]
 
 for row in tl.optimal_list(lists, '/Users/chibangnguyen/Documents/TiLearn/tests', '/Users/chibangnguyen/Documents/TiLearn/tests/backup'):
