c0a6314ff33c3193caa71edaa059c832340bdbb0
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-10 12:46:51 +0700

    chore

--- tests/test1.py
+++ tests/test1.py
@@ -1,16 +1,16 @@
 import tilearn as tl
 from tilearn import _plat as pl
 
-original_path = '/Users/chibangnguyen/Documents/TiLearn/tests'
-backup_path = '/Users/chibangnguyen/Documents/TiLearn/tests/backup'
-list1 = '/Users/chibangnguyen/Documents/TiLearn/tests/GTH-GTH.csv'
-list2 = '/Users/chibangnguyen/Documents/TiLearn/tests/DSA-DSA.csv'
-list3 = '/Users/chibangnguyen/Documents/TiLearn/tests/Tieng Anh-Tieng Anh.csv'
+original_path = '/Users/chibangnguyen/Documents/TiLearn/tests/personal-list'
+backup_path = '/Users/chibangnguyen/Documents/TiLearn/tests/personal-list/backup'
+list1 = '/Users/chibangnguyen/Documents/TiLearn/tests/personal-list/GTH-GTH.csv'
+list2 = '/Users/chibangnguyen/Documents/TiLearn/tests/personal-list/DSA-DSA.csv'
+list3 = '/Users/chibangnguyen/Documents/TiLearn/tests/personal-list/Tieng Anh-Tieng Anh.csv'
 
 lists = [
     pl.List(file_path=list1, prec=1),
-    pl.List(file_path=list2, prec=0),
-    pl.List(file_path=list3, prec=0)
+    pl.List(file_path=list2, prec=1),
+    pl.List(file_path=list3, prec=1)
 ]
 
 for row in tl.optimal_list(lists, original_path, backup_path):
---
+++ venv/lib/python3.12/site-packages/tilearn/_list/_bs 4.py
@@ -0,0 +1,50 @@
+import tilearn as tl
+from tilearn import data
+import os
+
+class List:
+    def __init__(self, data, prec=1):
+        self.data = data
+        self.prec = prec
+    
+    def info(self):
+        return self.data
+    
+    def run(self):
+        if self.prec == 1:
+            return tl.sum_factor(self.data)
+        elif self.prec == 0:
+            return tl.factor_data(self.data)
+    
+def job_amount_all(path, lists):
+    sum = 0
+    folder_path = path
+    csv_count = len([file for file in os.listdir(folder_path) if file.endswith('.csv')])
+    for i in range(csv_count):
+        count = sum + tl.job_amount(lists[i].info())
+        sum = count
+    return count
+
+def opt_loca(lists):
+    """
+    Returns
+    -------
+    opt = [sublist location, list row location]
+    """
+    opt = tl.list_gen(2, 1)
+    max_value = 0
+    position = 0
+    sl = -1
+    for sublist in lists:
+        sl += 1
+        opt[1][0] = sl
+        for i in range(tl.job_amount(sublist.info())):
+            if sublist.run()[i][5] > max_value:
+                max_value = sublist.run()[i][5]
+                position = i
+    return 
+
+def main_list(path, lists):
+    set_j = []
+    while job_amount_all(path) == 0:
+        a
