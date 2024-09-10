295856d7984eee2080a33432f55a635f61dbbb4b
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-10 12:41:21 +0700

    chore

---
+++ app/tests/backup 2.py
@@ -0,0 +1,56 @@
+import tilearn as tl
+from tilearn import _data as data
+from tilearn import _plat as pl
+import os
+import csv
+import shutil
+import glob
+
+# print(data.path('/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv'))
+# print(data.read_file('/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv'))
+# data.backup('/Users/chibangnguyen/Documents/TiLearn/app/tests', '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup')
+
+lists = [
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv', prec=0),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data2.csv', prec=0),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data3.csv', prec=0)
+]
+
+opt_list = lists[pl.location(lists, type='sub')].path
+print(opt_list)
+# print(pl.location(lists, type='sub'))
+
+# tl.optimal_list(lists, '/Users/chibangnguyen/Documents/TiLearn/app/tests', '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup')
+# for row in tl.optimal_list(lists, '/Users/chibangnguyen/Documents/TiLearn/app/tests', '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup'):
+#     print(row)
+
+# row_list = pl.location(lists, type='row')
+# opt_file = lists[pl.location(lists, type='sub')].path
+
+# print(row_list, opt_file)
+# print(data.prec(opt_file, row_list))
+# rows = data.none(opt_file, row_list)
+# print(rows)
+
+# data.updated(opt_file, prec=1, opt_row=row_list)
+# data.clear('/Users/chibangnguyen/Documents/TiLearn/app/tests/backup')
+
+# print(tl.set_const(lists, prec=1))
+
+
+# data.clear('/Users/chibangnguyen/Documents/TiLearn/app/tests/backup')
+
+# for row in tl.optimal_list(lists,'/Users/chibangnguyen/Documents/TiLearn/app/tests', '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup'):  
+#     print(row)
+
+# data.updated('/Users/chibangnguyen/Documents/TiLearn/app/tests/backup/data.csv', prec=1, opt_row=1)
+
+# print(pl.ja_all('/Users/chibangnguyen/Documents/TiLearn/app/tests', lists))
+# print(tl.set_const(lists, prec=1))
+
+# for sublist in lists:
+#     print(sublist.path())
+# print(lists[1].path)
+
+# for sublist in lists:
+#     print(sublist.path)
---
+++ app/tilearn/_list/_run 2.py
@@ -0,0 +1,64 @@
+import tilearn as tl
+from tilearn import _plat as pl
+from tilearn import _data as data
+import os
+
+def set_prec(lists, op, row):
+    set = tl.list_gen(row, 6)
+    # opt_list = lists[pl.location(lists, type='sub')].run()
+    for i in range(len(set)):
+        for j in range(len(set[i])):
+            if i == 0 and j == 0:
+                set[i][j] = str(op[i][j])
+            set[i][j] = op[i][j]
+    return set
+    
+def set(lists, op, row):
+    set = tl.list_gen(1, 6)
+    # opt_list = lists[pl.location(lists, type='sub')].run()
+    # opt_row = pl.location(lists, type='row')-1
+    fix_row = row
+    for i in range(len(set)):
+        for j in range(len(set[i])):
+            if i == 0 and j == 0:
+                set[i][j] = str(op[fix_row][j])
+            set[i][j] = op[fix_row][j]
+    return set
+
+def set_const(lists, prec, op, row):
+    if prec == 1:
+        return set_prec(lists, op, row)
+    elif prec == 0:
+        return set(lists, op, row)
+
+def file_seek(lists, path):
+    sum = pl.count_file(path)
+    for sublist in lists:
+        if sublist.info() == []:
+            sum -= 1
+    return sum
+
+def optimal_list(lists, path, backup_path):
+    set_j = []
+    data.backup(path, backup_path)
+    jc = pl.ja_all(lists, path)
+    while jc > 0:
+        opt_list = lists[pl.location(lists, type='sub')].run()
+        row_list = pl.location(lists, type='row')
+        opt_file = lists[pl.location(lists, type='sub')].path
+        check = lists[pl.location(lists, type='sub')].check()
+        if check == 1:
+            # if file_seek(lists, path=backup_path) == 1:
+            #     set_j.extend(opt_list)
+            #     break
+            set_j.extend(set_const(lists, prec=1, op=opt_list, row=row_list+1))
+            data.updated(opt_file, prec=1, opt_row=row_list)
+            jc -= row_list+1
+        elif check == 0:
+            set_j.extend(set_const(lists, prec=0, op=opt_list, row=row_list))
+            data.updated(opt_file, prec=0, opt_row=row_list)
+            jc -= 1
+        # print(file_seek(lists, path=backup_path))
+        # print(set_j, '\n')
+    data.clear(backup_path)
+    return set_j
