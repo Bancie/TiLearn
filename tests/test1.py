0fc0138a50a89fced4e9202a6f945c7a1e3a3881
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-10 12:26:33 +0700

    chore

--- tests/test1.py
+++ tests/test1.py
@@ -1,11 +1,17 @@
 import tilearn as tl
 from tilearn import _plat as pl
 
+original_path = '/Users/chibangnguyen/Documents/TiLearn/tests'
+backup_path = '/Users/chibangnguyen/Documents/TiLearn/tests/backup'
+list1 = '/Users/chibangnguyen/Documents/TiLearn/tests/GTH-GTH.csv'
+list2 = '/Users/chibangnguyen/Documents/TiLearn/tests/DSA-DSA.csv'
+list3 = '/Users/chibangnguyen/Documents/TiLearn/tests/Tieng Anh-Tieng Anh.csv'
+
 lists = [
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data2.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/tests/data3.csv', prec=0)
+    pl.List(file_path=list1, prec=1),
+    pl.List(file_path=list2, prec=0),
+    pl.List(file_path=list3, prec=0)
 ]
 
-for row in tl.optimal_list(lists, '/Users/chibangnguyen/Documents/TiLearn/tests', '/Users/chibangnguyen/Documents/TiLearn/tests/backup'):
+for row in tl.optimal_list(lists, original_path, backup_path):
     print(row)
