1f4bffeb1ea5fe50e888c3823f496d5365d4714d
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-11 16:28:14 +0700

    test

--- app/tests/backup.py
+++ app/tests/backup.py
@@ -1,15 +1,9 @@
 import tilearn as tl
-from tilearn import _data as data
 from tilearn import _plat as pl
-import os
-import csv
-import shutil
-import glob
 
 lists = [
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data2.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data3.csv', prec=0)
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv', prec=1),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data2.csv', prec=1),
 ]
 
 for row in tl.optimal_list(lists, '/Users/chibangnguyen/Documents/TiLearn/app/tests', '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup'):
