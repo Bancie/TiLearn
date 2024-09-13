9548e4178f0cc0ecc41de311727bef94e6f29cac
Bancie <144613141+Bancie@users.noreply.github.com>
2024-09-13 14:15:45 +0700

    chore

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 from tilearn._bs.basis import (
+    list_gen,
     job_amount,
-    jobscale,
-    process,
     p_factor_nonprec,
     p_factor_prec_tu,
     p_factor_prec,
-    list_gen,
-    calculate_process,
-    process_data,
     calculate_factor,
     factor_data,
+    c_time,
+    lateness,
+    sum_factor,
 )
 
 from tilearn._wspt.wspt import (
-    custom_wspt,
 )
