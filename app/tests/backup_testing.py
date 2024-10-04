1afa24788673bc866920f0acfd149d02702b7675
Bancie <144613141+Bancie@users.noreply.github.com>
2024-10-04 21:38:06 +0700

    chore

--- app/tests/backup_testing.py
+++ app/tests/backup_testing.py
@@ -5,9 +5,9 @@ tests = '/Users/chibangnguyen/Documents/TiLearn/app/tests'
 backup = '/Users/chibangnguyen/Documents/TiLearn/app/tests/backup'
 
 lists = [
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/folder1/data.csv', prec=0),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/folder1/data2.csv', prec=1),
-    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/folder1/data3.csv', prec=0)
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data.csv', prec=0),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data2.csv', prec=1),
+    pl.List(file_path='/Users/chibangnguyen/Documents/TiLearn/app/tests/data3.csv', prec=0)
 ]
 
 for row in tl.optimal_list(lists, tests, backup):
